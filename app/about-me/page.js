a8e0bc66d336a0dee0af96a3ff90cdc5d8974971
im23b-weberm <weberm@bzz.ch>
2025-03-13 18:38:00 +0100

    files for routing, header and footer

---
+++ app/about-me/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+      <div className="flex flex-row-reverse ...">
+          <div>01</div>
+          <div>02</div>
+          <div>03</div>
+      </div>
+  );
+}
