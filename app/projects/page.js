505e5acb0e1e434a06fef09f9d880626c72cb5a2
im23b-weberm <weberm@bzz.ch>
2025-04-06 18:37:38 +0200

    projects page

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,5 +1,37 @@
 export default function Projects() {
-     return(
-         <h1>Projects</h1>
-     );
+    return (
+        <div>
+            <div className="min-h-screen  mt-15 mb-20">
+                <h1 className="font-[Orbitron] text-3xl ml-25">Projects</h1>
+                <div className="flex flex-row place-content-between min-h-screen mt-13 ml-30 mr-30">
+                    <div className="bg-[#f5d4d4]  p-4 rounded-xl ">
+                        <img src="https://placehold.co/40x50" className="w-70 m-auto mt-2"/>
+                        <h1 className="text-xl font-semibold mt-5">Project X</h1>
+                        <p className="max-w-80 mb-5">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                            nonumy eirmod tempor invidunt ut
+                            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+                        <a href="#" className="hover:underline max-w-20 font-semibold">GitHub</a>
+                    </div>
+                    <div className="bg-[#f5d4d4]  p-4 rounded-xl ">
+                        <img src="https://placehold.co/40x50" className="w-70 m-auto mt-2"/>
+                        <h1 className="text-xl font-semibold mt-5">Project X</h1>
+                        <p className="max-w-80 mb-5">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                            nonumy eirmod tempor invidunt ut
+                            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+                        <a href="#" className="hover:underline max-w-20 font-semibold">GitHub</a>
+                    </div>
+                    <div className="bg-[#f5d4d4]  p-4 rounded-xl ">
+                        <img src="https://placehold.co/40x50" className="w-70 m-auto mt-2"/>
+                        <h1 className="text-xl font-semibold mt-5">Project X</h1>
+                        <p className="max-w-80 mb-5">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                            nonumy eirmod tempor invidunt ut
+                            labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+                        <a href="#" className="hover:underline max-w-20 font-semibold">GitHub</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    );
 }
